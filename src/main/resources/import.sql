2d1e01fe19b5e495a6413e302bff8d205a30e03e
cristian.pardo <cpardo@kiosolutions.com>
2020-01-10 17:04:11 -0300

    Muestra datos usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 INSERT INTO `user` (create_at, email, enabled, first_name, last_name, password, social_id, username) VALUES(NULL, 'admin@admin.cl', true, 'Admin', 'admin', '$2a$10$m7IuhSw2vUrZWUkVq/M/tu8BQBOIwrNWWhQh04CcbWMjdFPWVa6Yy', '172132332', 'admin');
+INSERT INTO `user` (create_at, email, enabled, first_name, last_name, password, social_id, username) VALUES(NULL, 'user@admin.cl', true, 'User', 'User', '$2a$10$m7IuhSw2vUrZWUkVq/M/tu8BQBOIwrNWWhQh04CcbWMjdFPWVa6Yy', '168946430', 'user');
 
 INSERT INTO `role` (name) VALUES('ROLE_ADMIN');
 
